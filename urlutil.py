1898fc46819caed9ae2b3cbbd6160a754151c225
Rokoucha <git@rokoucha.net>
2021-07-31 17:00:47 +0900

    fix: base64 エンコーディング時のエラーを修正

--- urlutil.py
+++ urlutil.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import base64
 import re
 
 # urllib2.Request で開く url と headers を生成する
@@ -14,7 +15,7 @@ def getUrlInfo(server_url):
     if match is not None:
         # BASIC 認証
         result["url"] = match.group(1) + "://" + match.group(4);
-        result["headers"]["authorization"] = "Basic " + (match.group(2) + ":" + match.group(3)).encode("base64")[:-1]
+        result["headers"]["authorization"] = "Basic " + base64.b64encode((match.group(2) + ":" + match.group(3)).encode()).decode();
     else:
         match = re.search(r"^(http|https)://(.+)", server_url)
         result["url"] = match.group(1) + "://" + match.group(2);
